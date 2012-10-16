6b76ee5d23bc6143bf7fd8ea1fb5be8bc04ed8ba
Harshavardhana <harsha@harshavardhana.net>
2012-10-15 18:02:15 -0700

    Refactor v2

--- extractor/main.py
+++ extractor/main.py
@@ -30,7 +30,8 @@ class Extractor:
         self.RETRY_LENGTH = 250
         logger.setLevel(level=self.options['loglevel'])
 
-    def _html(self, force=False):
+    def normalize_html(self, force=False):
+        # Use lxml 'Cleaner' class to normalize html to a feasible value
         if force or self.html is None:
             cleaner = Cleaner(scripts=True, javascript=True, comments=True,
                               style=True, links=True, meta=False, add_nofollow=False,
@@ -45,28 +46,30 @@ class Extractor:
         return self.html
 
     def content(self):
-        return get_body(self._html())
+        return get_body(self.normalize_html())
 
     def title(self):
-        return get_title(self._html())
+        return get_title(self.normalize_html())
 
-    def summary(self):
+    def extracted(self):
         try:
-            ruthless = True
+            single_pass = True
             while True:
                 self._html(True)
                 [i.drop_tree() for i in self.tags(self.html, 'script', 'style', 'noscript')]
 
-                if ruthless: self.remove_unlikely_candidates()
-                self.transform_misused_divs_into_paragraphs()
-                candidates = self.score_paragraphs(self.options.get('min_text_length', self.TEXT_LENGTH_THRESHOLD))
+                if single_pass:
+                    self.remove_unlikely_nodes()
 
-                best_candidate = self.select_best_candidate(candidates)
-                if best_candidate:
-                    article = self.get_article(candidates, best_candidate)
+                self.transform_misused_divs_into_paragraphs()
+                nodes = self.score_paragraphs(self.options.get('min_text_length', self.TEXT_LENGTH_THRESHOLD))
+
+                best_node = self.select_best_node(nodes)
+                if best_node:
+                    article = self.get_article(nodes, best_node)
                 else:
-                    if ruthless:
-                        ruthless = False
+                    if single_pass:
+                        single_pass = False
                         self.debug("Ended up stripping too much - going for a safer parsing scheme")
                         # try again
                         continue
@@ -79,22 +82,23 @@ class Extractor:
 
                 content_scores = []
                 best_score = 0.0
-                new_candidate = None
+                new_node = None
 
-                for x in candidates:
-                    if candidates[x]['content_score'] < 0:
+                for x in nodes:
+                    if nodes[x]['content_score'] < 0:
                         continue
-                    content_scores.append(candidates[x]['content_score'])
+                    content_scores.append(nodes[x]['content_score'])
 
                 try:
                     best_score = sorted(content_scores)[len(content_scores)-1:][0]
                 except Exception as e:
                     pass
 
-                cleaned_article = self.sanitize(article, candidates)
-                of_acceptable_length = len(cleaned_article or '') >= (self.options['retry_length'] or self.RETRY_LENGTH)
-                if ruthless and not of_acceptable_length:
-                    ruthless = False
+                cleaned_article = self.sanitize(article, nodes)
+                at_acceptable_length = len(cleaned_article or '') >= (self.options['retry_length'] or self.RETRY_LENGTH)
+
+                if single_pass and not at_acceptable_length:
+                    single_pass = False
                     continue # try again
                 else:
                     return cleaned_article
@@ -106,18 +110,18 @@ class Extractor:
             logger.exception('error getting summary: ' )
             return None
 
-    def get_article(self, candidates, best_candidate):
-        # Now that we have the top candidate, look through its siblings for content that might also be related.
+    def get_article(self, nodes, best_node):
+        # Now that we have the top node, look through its siblings for content that might also be related.
         # Things like preambles, content split by ads that we removed, etc.
 
-        sibling_score_threshold = max([10, best_candidate['content_score'] * 0.2])
+        sibling_score_threshold = max([10, best_node['content_score'] * 0.2])
         output = parse("<div/>")
-        for sibling in best_candidate['elem'].getparent().getchildren():
+        for sibling in best_node['elem'].getparent().getchildren():
             append = False
-            if sibling is best_candidate['elem']:
+            if sibling is best_node['elem']:
                 append = True
             sibling_key = sibling
-            if sibling_key in candidates and candidates[sibling_key]['content_score'] >= sibling_score_threshold:
+            if sibling_key in nodes and nodes[sibling_key]['content_score'] >= sibling_score_threshold:
                 append = True
 
             if sibling.tag == "p":
@@ -132,21 +136,21 @@ class Extractor:
 
             if append:
                 output.append(sibling)
-        if output is not None: output.append(best_candidate['elem'])
+        if output is not None: output.append(best_node['elem'])
         return output
 
-    def select_best_candidate(self, candidates):
-        sorted_candidates = sorted(candidates.values(), key=lambda x: x['content_score'], reverse=True)
-        self.debug("Top 5 candidates:")
-        for candidate in sorted_candidates[:5]:
-            elem = candidate['elem']
-            self.debug("Candidate %s with score %s '%s...'" % (describe(elem), candidate['content_score'], snippet(elem)))
+    def select_best_node(self, nodes):
+        sorted_nodes = sorted(nodes.values(), key=lambda x: x['content_score'], reverse=True)
+        self.debug("Top 5 nodes:")
+        for node in sorted_nodes[:5]:
+            elem = node['elem']
+            self.debug("Node %s with score %s '%s...'" % (describe(elem), node['content_score'], snippet(elem)))
 
-        if len(sorted_candidates) == 0:
+        if len(sorted_nodes) == 0:
             return None
-        best_candidate = sorted_candidates[0]
-        self.debug("Best candidate %s with score %s" % (describe(best_candidate['elem']), best_candidate['content_score']))
-        return best_candidate
+        best_node = sorted_nodes[0]
+        self.debug("Best node %s with score %s" % (describe(best_node['elem']), best_node['content_score']))
+        return best_node
 
     def get_link_density(self, elem):
         link_length = len("".join([i.text_content() or "" for i in elem.findall(".//a")]))
@@ -154,7 +158,7 @@ class Extractor:
         return float(link_length) / max(text_length, 1)
 
     def score_paragraphs(self, min_text_length):
-        candidates = {}
+        nodes = {}
         self.debug(str([describe(node) for node in self.tags(self.html, "div")]))
         elems = self.tags(self.html, "div", "p", "td", 'li', "a")
 
@@ -171,30 +175,33 @@ class Extractor:
             if (not inner_text) or len(inner_text) < min_text_length:
                 continue
 
-            if parent_key not in candidates:
-                candidates[parent_key] = self.score_node(parent_node)
-            if grand_parent_node is not None and grand_parent_key not in candidates:
-                candidates[grand_parent_key] = self.score_node(grand_parent_node)
+            if parent_key not in nodes:
+                nodes[parent_key] = self.score_node(parent_node)
+            if grand_parent_node is not None and grand_parent_key not in nodes:
+                nodes[grand_parent_key] = self.score_node(grand_parent_node)
 
             content_score = 1
             content_score += len(inner_text.split(','))
             content_score += min([(len(inner_text) / 100), 3])
-            if elem not in candidates:
-                candidates[elem_key] = self.score_node(elem)
-            candidates[elem_key]['content_score'] += content_score
-            candidates[parent_key]['content_score'] += content_score
+            if elem not in nodes:
+                nodes[elem_key] = self.score_node(elem)
+            nodes[elem_key]['content_score'] += content_score
+            nodes[parent_key]['content_score'] += content_score
             if grand_parent_node is not None:
-                candidates[grand_parent_key]['content_score'] += content_score / 2.0
+                nodes[grand_parent_key]['content_score'] += content_score / 2.0
 
-        # Scale the final candidates score based on link density. Good content should have a
+        # Scale the final nodes score based on link density. Good content should have a
         # relatively small link density (5% or less) and be mostly unaffected by this operation.
-        for elem, candidate in candidates.items():
+        for elem, node in nodes.items():
             link_density = self.get_link_density(elem)
-            candidate['content_score'] *= (1 - link_density)
-            if candidate['content_score'] > 0:
-                self.debug("candidate %s scored %s (linkd: %s) '%s'" % (describe(elem), candidate['content_score'], link_density, snippet(elem,30)))
+            node['content_score'] *= (1 - link_density)
+            if node['content_score'] > 0:
+                self.debug("node %s scored %s (linkd: %s) '%s'" % (describe(elem),
+                                                                        node['content_score'],
+                                                                        link_density,
+                                                                        snippet(elem,30)))
 
-        return candidates
+        return nodes
 
     def class_weight(self, e):
         weight = 0
@@ -230,13 +237,13 @@ class Extractor:
     def debug(self, *a):
             logger.debug(*a)
 
-    def remove_unlikely_candidates(self):
+    def remove_unlikely_nodes(self):
         remove_list = []
         context = iterwalk(self.html)
         for action, elem in context:
             s = "%s%s" % (elem.get('class', ''), elem.get('id', ''))
-            if REGEXPS['unlikelyCandidates'].search(s) and (not REGEXPS['okMaybeItsACandidate'].search(s)) and elem.tag != 'body':
-                self.debug("Removing unlikely candidate - %s" % (s,))
+            if REGEXPS['unlikelyNodes'].search(s) and (not REGEXPS['okMaybeItsANode'].search(s)) and elem.tag != 'body':
+                self.debug("Removing unlikely node - %s" % (s,))
                 remove_list.append(elem)
         [e.drop_tree() for e in remove_list if e.tag != 'html']
 
@@ -253,7 +260,7 @@ class Extractor:
             for e in node.findall('.//%s' %tag_name):
                 yield e
 
-    def sanitize(self, node, candidates):
+    def sanitize(self, node, nodes):
         for header in self.tags(node, "h1", "h2", "h3", "h4", "h5", "h6"):
             if self.class_weight(header) < 0 or self.get_link_density(header) > 0.33: header.drop_tree()
 
@@ -266,8 +273,8 @@ class Extractor:
                 continue
             weight = self.class_weight(el)
             el_key = el
-            if el_key in candidates:
-                content_score = candidates[el_key]['content_score']
+            if el_key in nodes:
+                content_score = nodes[el_key]['content_score']
             else:
                 content_score = 0
 
@@ -286,8 +293,8 @@ class Extractor:
                 link_density = self.get_link_density(el)
                 parent_node = el.getparent()
                 if parent_node is not None:
-                    if parent_node in candidates:
-                        content_score = candidates[parent_node]['content_score']
+                    if parent_node in nodes:
+                        content_score = nodes[parent_node]['content_score']
                     else:
                         content_score = 0
                     pweight = self.class_weight(parent_node) + content_score
